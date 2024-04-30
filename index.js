cd6256586281ad7f677519bc3c0b846b9d26bce3
icoderdoc <coderdoc@yandex.ru>
2024-05-01 00:25:50 +0300

    output lists of unique values

--- index.js
+++ index.js
@@ -11,33 +11,26 @@ const result = decoded(encoded, translations);
 // console.log(result);
 outputHash(result);
 
-// выводим список уникальных groupId:
-uniqueValuesOutput(result, "groupId");
-// выводим список уникальных areaId:
-uniqueValuesOutput(result, "areaId");
-// выводим список уникальных departmentId:
-uniqueValuesOutput(result, "departmentId");
-// выводим список уникальных directionId:
-uniqueValuesOutput(result, "directionId");
-// выводим список уникальных mediaTypeId:
-uniqueValuesOutput(result, "mediaTypeId");
-// выводим список уникальных mediaId:
-uniqueValuesOutput(result, "mediaId");
-// выводим список уникальных resellingId:
-uniqueValuesOutput(result, "resellingId");
-// выводим список уникальных serviceTypeId:
-uniqueValuesOutput(result, "serviceTypeId");
-// выводим список уникальных formatTypeId:
-uniqueValuesOutput(result, "formatTypeId");
-// выводим список уникальных formatId:
-uniqueValuesOutput(result, "formatId");
-// выводим список уникальных unitId:
-uniqueValuesOutput(result, "unitId");
-// выводим список уникальных platformId:
-uniqueValuesOutput(result, "platformId");
-// выводим список уникальных budgetId:
-uniqueValuesOutput(result, "budgetId");
-// выводим список уникальных adPlatformId:
-uniqueValuesOutput(result, "adPlatformId");
-// выводим список уникальных mpmId:
-uniqueValuesOutput(result, "mpmId");
+// output list of unique values
+const listId = [
+  "groupId",
+  "areaId",
+  "departmentId",
+  "directionId",
+  "mediaTypeId",
+  "mediaId",
+  "resellingId",
+  "serviceTypeId",
+  "formatTypeId",
+  "formatId",
+  "unitId",
+  "platformId",
+  "budgetId",
+  "adPlatformId",
+  "mpmId",
+];
+
+for (let i = 0; i < listId.length; i++) {
+  console.log(listId[i]);
+  uniqueValuesOutput(result, listId[i]);
+}
